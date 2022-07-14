1e6e92633653b02b8aa75f9791450b954393d0e0
Marcel Seelen <anna-marie.seelen@wur.nl>
2022-07-14 16:24:32 +0200

    added comments to make everything more clear, ghehe

--- make_pdf_with_smiles.py
+++ make_pdf_with_smiles.py
@@ -29,15 +29,14 @@ def connection_Mass2Motifs_to_documents(path_file_with_MS2LDA_csv):
     #add Motif and Document number into 1 dataframe, but only include Motifs with more than 5 doc --> you will lose some doc
     df_MS2LDA["Document+Probability+Overlap"] = df_MS2LDA[["Document", "Probability", "Overlap Score"]].values.tolist()
     df_new2 = df_MS2LDA[["Motif", "Document+Probability+Overlap"]].groupby("Motif", as_index=True).aggregate({"Document+Probability+Overlap":list})
-    #TODO: think about if this is even usefull to have in the document the prob and overlapscore
     df_new2 = df_new2[df_new2["Document+Probability+Overlap"].str.len() >= 5]
     df_new=df_MS2LDA[["Motif","Document"]].groupby("Motif", as_index=True).aggregate({"Document":list})
     # we only want the motifs found in more than 5 spectra
     df_new=df_new[df_new['Document'].str.len() >= 5]
-    #https://stackoverflow.com/questions/58297277/how-to-get-a-length-of-lists-in-pandas-dataframe
+    # solution from: https://stackoverflow.com/questions/58297277/how-to-get-a-length-of-lists-in-pandas-dataframe
     f = lambda x: 'document_{}'.format(x + 1)
     df= pd.DataFrame(df_new.Document.values.tolist(),df_new.index, dtype=object).fillna('').rename(columns=f)
-    # https://stackoverflow.com/questions/44663903/pandas-split-column-of-lists-of-unequal-length-into-multiple-columns
+    # solution from: https://stackoverflow.com/questions/44663903/pandas-split-column-of-lists-of-unequal-length-into-multiple-columns
     df = pd.concat([df, df_new2], axis=1)
     return df
 
@@ -66,20 +65,7 @@ def information_document_Mass2Motif_node(path_file_with_MS2LDA_csv_fragments):
     df_new=pd.concat([df_new, df_new2], axis=1)
     return df_new
 
-def make_df_smiles(df_doc_to_smiles):
-    #TODO: waarom doet deze smiles het niet?
-    data_smiles=[]
-    for index,row in df_doc_to_smiles.iterrows():
-        if row["smiles"] != "":
-            data_smiles.append(row)
-        else:
-            pass
-    df_smiles = pd.DataFrame(data_smiles)
-    print(df_smiles)
-    return df_smiles
-
 def visualize_mol(smiles):
-    #TODO: doesn't work for some reason: cannot put it in pdf
     """
     function adapted from: https://pchanda.github.io/See-substructure-in-molecule/
     :param smiles:
@@ -92,77 +78,63 @@ def visualize_mol(smiles):
     img = MolToImage(mol, size=(200, 200), fitImage=True)
     return img
 
-def visualize_on_command_line(img):
-    """Run jellyfish program on fasta file
-    input_fn: string, filename of input FASTA file
-    kmer_size: int, size of k-mers used by jellyfish
-    """
-    #out_fn = 'tomato{}'.format(kmer_size)
-    cmd = 'viu {}' \
-            .format(img)
-    # e = subprocess.check_output(cmd, shell=True)
-    # if os.path.exists(out_fn):
-    #     cmd = 'jellyfish stats {}'.format(out_fn)
-    # else:
-    #     cmd = 'jellyfish stats {}_0'.format(out_fn)
-    res = subprocess.check_output(cmd, shell=True)
-    return res
-
 def main() -> None:
     """Main function of this module"""
-    # step 1: parse MS2Query file
     path_to_file_with_MS2Query_csv= argv[1]
-    # step 2: parse MS2LDA file
     path_file_with_MS2LDA_csv = argv[2]
-    # step 3: input Mass2Motif fragments file
     path_file_with_Motif_fragments_csv = argv[3]
-    # stept 3: put relevant info in new document
+    # step 1: rearrange MS2LDA file with mass2motifs and documents in dataframe
     df_motifs_to_doc=connection_Mass2Motifs_to_documents(path_file_with_MS2LDA_csv)
-    print(df_motifs_to_doc)
+    #step 2: rearrange MS2Query file with documents and smiles of analogs in dataframe
     df_doc_to_smiles=information_document_node(path_to_file_with_MS2Query_csv)
-    print(df_doc_to_smiles)
+    # step 3: rearrange MS2LDA fragment file with mass2motifs and fragments in dataframe
     df_motifs_to_frag=information_document_Mass2Motif_node(path_file_with_Motif_fragments_csv)
-    # step 5: make dataframe with only smiles
-    df_smiles=make_df_smiles(df_doc_to_smiles)
-    #step 6: function to visualize smiles
-    #print each mass2Motif with information
-
+    #step 4: make a PDF where you can see each Mass2Motif, its fragments, and the associated analog structures
     pdf=FPDF()
     pdf.add_page()
     pdf.set_font("helvetica", size=10)
     for index, row in df_motifs_to_doc.iterrows():
-        print(row)
-        amount_of_documents = []
-        pdf.multi_cell(200,5, txt="\n", align = 'C')
-        pdf.set_font("helvetica", "B", size=10)
-        pdf.multi_cell(200, 5, txt="{0}\n".format(index), align = 'C')
-        pdf.set_font("helvetica", size=10)
-        #if index in df_motifs_to_frag.index.values.tolist():
-        if index in df_motifs_to_frag.index.values.tolist():
-            pdf.multi_cell(200, 5, txt="{0}\n".format(df_motifs_to_frag.at[index, "Fragment+Probability"]), align = 'L')
-        else:
-            pdf.multi_cell(200, 5, txt="motif does not have fragments with probability > 0.05", align = 'L')
-        pdf.multi_cell(200, 5, txt="[Document, probability, overlap score]\n", align='L')
-        pdf.multi_cell(200, 5, txt="{0}\n".format(df_motifs_to_doc.at[index, "Document+Probability+Overlap"]), align = 'L') # motif_398 is not in df_motifs_to_frag which is weird.... because GNPS and MS2LDA
-        pdf.multi_cell(200, 5, txt="amount of spectra associated with motif:{0}\n".format(len(df_motifs_to_doc.at[index, "Document+Probability+Overlap"])), align = 'L')
-        amount_of_smiles=0
+        amount_of_smiles = 0
+        # calculating the amount of smiles with a probability > 0.7 associated with each motif
         for cell in row:
-            if cell in df_smiles.index.values.tolist():
+            if cell in df_doc_to_smiles.index.values.tolist():
                 if df_doc_to_smiles.at[cell, "smiles"] != "":
                     amount_of_smiles+=1
-        for cell in row:
-            if isinstance(cell, int):
-                if amount_of_smiles>=2:
+        # selection of printed Mass2Motifs based on the amount of smiles with a probability > 0.7. At least 2 or more Smiles are needed.
+        if amount_of_smiles >= 2:
+            pdf.multi_cell(200,5, txt="\n", align = 'C')
+            pdf.set_font("helvetica", "B", size=10)
+            # writing the mass_2_motif name, which is the index of df_motifs_to_doc in the file
+            pdf.multi_cell(200, 5, txt="{0}\n".format(index), align = 'C')
+            pdf.set_font("helvetica", size=10)
+            # if the mass2motif is in the list of indexes of df_motifs_to_frag then look for the column with fragments + probability of that mass2motif and sort that list on the probability value
+            if index in df_motifs_to_frag.index.values.tolist():
+                pdf.multi_cell(200, 5, txt="{0}\n".format(sorted(df_motifs_to_frag.at[index, "Fragment+Probability"], key = lambda x: x[1], reverse=True)), align = 'L')
+            else:
+                pdf.multi_cell(200, 5, txt="motif does not have fragments with probability > 0.05", align = 'L')
+            # print the "Document+probability+Overlap" column for the selected index, which is the selected Mass2Motif
+            pdf.set_font("helvetica", "I", size=10)
+            pdf.multi_cell(200, 5, txt="[[Spectrum number, probability, overlap score], [Spectrum number, probability, overlap score], etc.] :\n", align='L')
+            pdf.set_font("helvetica", size=10)
+            pdf.multi_cell(200, 5, txt="{0}\n".format(df_motifs_to_doc.at[index, "Document+Probability+Overlap"]), align = 'L')
+            # print the amount of lists in the "Document+probability+Overlap" column for the selected index, which is equal to the amount of documents the motif is associated with
+            pdf.multi_cell(200, 5, txt="amount of spectra associated with motif: {0}\n".format(len(df_motifs_to_doc.at[index, "Document+Probability+Overlap"])), align = 'L')
+            # for each individual cell in df_motifs_to_doc, which is the document number associated with the index, so the mass2motif
+            for cell in row:
+                # if the cell value is an int, so if its really a spectrum number (because there is also a list of lists in this dataframe)
+                if isinstance(cell, int):
+                    # if the spectrum number is in the index of df_doc_to_smiles
                     if cell in df_doc_to_smiles.index.values.tolist():
+                        # if the smiles associated with the spectrum number is not empty, because most cells are empty because of a too low prediction probability of the analogue
                         if df_doc_to_smiles.at[cell, "smiles"] != "":
-                            pdf.multi_cell(200, 10, txt="spectrum number:{0}, {1} m/z\n".format(cell, df_doc_to_smiles.at[cell, "precursor_mz_query_spectrum"]),align='L')
-                    if cell in df_smiles.index.values.tolist():
-                        pdf.multi_cell(200, 10, txt="{0} m/z\n".format(df_doc_to_smiles.at[cell, "precursor_mz_query_spectrum"]),align='L')
-                        pdf.image(visualize_mol(df_smiles.at[cell, "smiles"]))
-                        pdf.multi_cell(200, 10, txt="{0} m/z, prediction {1}\n".format(df_doc_to_smiles.at[cell, "precursor_mz_analog"], df_doc_to_smiles.at[cell, "ms2query_model_prediction"] ), align = 'L')
-                #TODO:script: mass2motifs selecteren op de hoeveelheid smiles die er zijn gevonden ervan
-            #if cell in df_motifs_to_doc.index.values.tolist():
-                #pdf.multi_cell(200, 5, txt="{0}\n".format(df_motifs_to_doc.at[cell, "Document+Probability+Overlap"]), align='L')
+                            # then print the associated spectrum number that the smiles belongs to and the precursor m/z of the spectrum
+                            pdf.multi_cell(200, 10, txt="spectrum number: {0}, precursor m/z: {1}\n".format(cell, df_doc_to_smiles.at[cell, "precursor_mz_query_spectrum"]),align='L')
+                            # also use the fuction visualize_mol to print a picture of the structure of the analog using the smiles that is associated with the spectrum
+                            pdf.image(visualize_mol(df_doc_to_smiles.at[cell, "smiles"]))
+                            # print the predicition probability of the analog, and the mz of the smiles that is associated with the spectrum
+                            pdf.multi_cell(200, 10, txt="analog m/z:{0}, prediction: {1}\n".format(
+                                df_doc_to_smiles.at[cell, "precursor_mz_analog"],
+                                df_doc_to_smiles.at[cell, "ms2query_model_prediction"]), align='L')
     pdf.output("output.pdf")
 
 if __name__ == "__main__":
