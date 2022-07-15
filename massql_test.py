124bbcf2f496d0adb75cef5eec2d56cbde924825
Marcel Seelen <anna-marie.seelen@wur.nl>
2022-07-15 15:40:59 +0200

    tried some new stuff but still doesn't work

--- massql_test.py
+++ massql_test.py
@@ -15,25 +15,41 @@ Usage: python3 *name_of_script* *name_input_file* [*output_file*] [gap_penalty]
 from massql import msql_engine
 from sys import argv
 import sys
-sys.path.insert(1, '/lustre/BIF/nobackup/seele006/MS2Query_search_libraries')
 #TODO: fix that it imports the file, because the massql function sucksss
-import ALL_GNPS_210409_positive_processed_annotated_CF_NPC_classes.txt
+#import ALL_GNPS_210409_positive_processed_annotated_CF_NPC_classes.txt
 import ntpath
+from pathlib import Path
+import pyarrow.feather as feather
+import os
 
 # functions
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
 
 def try_massql(query, file):
-    results_df=msql_engine.process_query(query,file) # this function can only look in the pwd, but this function should look in head
-    return results_df
+    #sys.path.insert(1, '/lustre/BIF/nobackup/seele006/MS2Query_search_libraries')
+    results=msql_engine.process_query(query,file)
+    return results
+
+def from_feather_to_df(results_feather):
+    read_df = feather.read_feather(results_feather)
+    print(read_df)
+    return read_df
 
 def main():
     """Main function of this module"""
     # step 1: parse the protein sequence from file 1 and file 2 into dict
-    head, tail = ntpath.split(argv[1])
-    path_to_file_with_GNPS_mass_library_txt = argv[1]
+    path=argv[1]
+    name_file="FractionProfiling_RPOS_ToF10_PreCheck_LTR_01_DDA.mzML"
+    print(find(path, name_file))
+    #head, tail = ntpath.split(argv[1])
+    #path_to_file_with_GNPS_mass_library_txt = argv[1]
     query=("QUERY scaninfo(MS2DATA) WHERE MS2NL=163")
-    file="ALL_GNPS_210409_positive_processed_annotated_CF_NPC_classes.txt"
-    try_massql(query, file)
+    file = "FractionProfiling_RPOS_ToF10_PreCheck_LTR_01_DDA.mzML"
+    results_feather=try_massql(query, file)
+    from_feather_to_df(results_feather)
 
 
 if __name__ == "__main__":
