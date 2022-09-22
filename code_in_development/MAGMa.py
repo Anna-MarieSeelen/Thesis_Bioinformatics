046c71e5ecb577e6d1504369c96011ab51f82506
Marcel Seelen <anna-marie.seelen@wur.nl>
2022-09-22 12:18:52 +0200

    added functions to excute command on line

--- code_in_development/MAGMa.py
+++ code_in_development/MAGMa.py
@@ -19,6 +19,7 @@ import re
 from pathlib import Path
 import os
 import subprocess
+import sys
 
 #functions
 def convert_json_to_sqlite(path_to_json_file,dir_database):
@@ -74,17 +75,38 @@ def initialize_db_to_save_results(path_to_store_results_db,path_to_spectrum_file
     #generate a good database name with the spectrum id and motif in it.
     spectrum_name=re.search(r'(.*)(spectrum.*)(.txt)', path_to_spectrum_file).group(2)
     file_path_out = Path(r"{0}/MAGMa_db_{1}.sqlite".format(path_to_store_results_db, spectrum_name))
-    #if you try to annotate something in an exisiting database it will go wrong, so if the database exists remove it
+    #if you try to annotate something in an existing database it will go wrong, so if the database exists remove it.
     if os.path.exists(file_path_out):
         os.remove(file_path_out)
     cmd = 'magma init_db {0}'\
             .format(file_path_out)
-    e = subprocess.check_call(cmd, shell=True)
-    print("EXIT STATUS AND TYPE", e, type(e))
+    try:
+        e = subprocess.check_call(cmd, shell=True, stdout = subprocess.DEVNULL, stderr = subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        pass
     return file_path_out
 
-def read_spectrum_to_be_annotated_into_db(path_to_spectrum_file):
+def add_spectrum_to_be_annotated_into_db(path_to_results_db_file, path_to_spectrum_file, abs_intensity_thres=1000, mz_precision_ppm=80, mz_precision_abs=0.01, spectrum_file_type='mgf', ionisation=1):
+    # a = Absolute intensity threshold for storing peaks in database (default: 1000)
+    # i = Ionisation mode (default: 1)
+    # p= Maximum relative m/z error (ppm) (default: 5)
+    # q= Maximum absolute m/z error (Da) (default: 0.001)
+    cmd = 'magma read_ms_data -f {0} -i {1} -a {2} -p {3} -q {4} {5} {6}'.format(spectrum_file_type,ionisation,abs_intensity_thres, mz_precision_ppm, mz_precision_abs, path_to_spectrum_file, path_to_results_db_file)
+    e = subprocess.check_call(cmd, shell=True,stdout = subprocess.DEVNULL, stderr = subprocess.STDOUT)
+    #print("EXIT STATUS AND TYPE", e, type(e))
+    return None
 
+def annotate_spectrum(path_to_structures_database,path_to_results_db_file, max_num_break_bonds=10, structure_db="hmdb", ncpus=1):
+    # b = Maximum number of bond breaks to generate substructures (default: 3)
+    # s= --structure_database {pubchem,kegg,hmdb} Retrieve molecules from structure database (default: )
+    # n = Number of parallel cpus to use for annotation (default: 1)
+    cmd = "magma annotate -b {0} -s {1} -o {2} -n {3} {4}".format(max_num_break_bonds,structure_db,path_to_structures_database,ncpus, path_to_results_db_file)
+    e = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    # e=e.decode('utf-8')
+    # for i in e.split('\n'):
+    #     print(i)
+    # if you want to do something with the output
+    return None
 
 #TODO: massql will return a sqlite database: you should check the identifiers! Definitely look at the HMDB identifier
 
@@ -95,8 +117,16 @@ def main():
     path_to_structures_database=argv[1]
     path_to_spectrum_file=argv[2]
     path_to_store_results_db=argv[3]
-    #step 1:
-    print(initialize_db_to_save_results(path_to_store_results_db, path_to_spectrum_file))
+    #step 1: initialize database to save results
+    path_to_results_db_file=initialize_db_to_save_results(path_to_store_results_db, path_to_spectrum_file)
+    print(path_to_results_db_file)
+    # step 2: add spectrum to be annotated into the results database
+    add_spectrum_to_be_annotated_into_db(path_to_results_db_file, path_to_spectrum_file, abs_intensity_thres=1000,
+                                         mz_precision_ppm=80, mz_precision_abs=0.01, spectrum_file_type='mgf',
+                                         ionisation=1)
+    #step 3: annotate spectrum and store output in results database
+    annotate_spectrum(path_to_structures_database, path_to_results_db_file, max_num_break_bonds=10, structure_db="hmdb",
+                      ncpus=1)
 
 if __name__ == "__main__":
     main()
