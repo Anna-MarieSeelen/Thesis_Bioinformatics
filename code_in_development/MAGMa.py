6d19ae3683fe5fc0afeb286398071627244245bf
Marcel Seelen <anna-marie.seelen@wur.nl>
2022-09-23 12:18:32 +0200

    added some functions to process output dataframe

--- code_in_development/MAGMa.py
+++ code_in_development/MAGMa.py
@@ -20,6 +20,10 @@ from pathlib import Path
 import os
 import subprocess
 import sys
+from decimal import *
+import ast
+from matchms import Spectrum
+from matchms.filtering import add_losses
 
 #functions
 def convert_json_to_sqlite(path_to_json_file,dir_database):
@@ -108,7 +112,6 @@ def annotate_spectrum(path_to_structures_database,path_to_results_db_file, max_n
     # if you want to do something with the output
     return None
 
-#TODO: massql will return a sqlite database: you should check the identifiers! Definitely look at the HMDB identifier
 def get_molid_of_matched_compound(path_to_results_db_file, identifier_spectrum):
     conn = sqlite3.connect(path_to_results_db_file)
     sqlite_command = \
@@ -123,7 +126,7 @@ def get_molid_of_matched_compound(path_to_results_db_file, identifier_spectrum):
         if identifier_spectrum==str(re.search(r'.*\((HMDB.*)\).*', identifier).group(1)):
             conn = sqlite3.connect(path_to_results_db_file)
             sqlite_command = \
-                f"""SELECT molid FROM molecules WHERE name = 'L-Aspartic acid (HMDB0000191)'"""
+                f"""SELECT molid FROM molecules WHERE name = '{identifier}'"""
             cur = conn.cursor()
             cur.execute(sqlite_command)
             molid=cur.fetchall()
@@ -133,11 +136,85 @@ def get_molid_of_matched_compound(path_to_results_db_file, identifier_spectrum):
             pass
             return None
 
-def blabla():
+def make_spectrum_object(molid,path_to_results_db_file):
+    conn = sqlite3.connect(path_to_results_db_file)
+    sqlite_command = \
+        f"""SELECT mz,smiles FROM fragments WHERE molid = '{molid}'"""
+    cur = conn.cursor()
+    cur.execute(sqlite_command)
+    list_with_fragments_and_smiles=cur.fetchall()
+    return list_with_fragments_and_smiles
+
+def parse_line_with_motifs_and_querries(line):
+    motif=re.search(r'(.*)    (.*)    (.*)', line).group(1)
+    feature_list=re.search(r'(.*)    (.*)    (.*)', line).group(2)
+    query=re.search(r'(.*)    (.*)    (.*)', line).group(3)
+    return motif,feature_list,query
+
+def look_for_features(txt_file_with_motif_and_frag, current_motif):
+    lines = (open(txt_file_with_motif_and_frag))
+    print("opened file")
+    for line in lines:
+        line = line.strip()
+        line = line.replace('\n', '')
+        motif, features_list, query = parse_line_with_motifs_and_querries(line)
+        features_list_1 = ast.literal_eval(features_list)
+        if motif == current_motif:
+            list_of_features=[]
+            for object in features_list_1:
+                list_of_features.append(object[0])
+            return(list_of_features)
+
+def select_features(list_of_features,list_with_fragments_and_smiles):
+    for feature in list_of_features:
+        if re.search(r'loss', feature[0]) != None:
+            # first calculate all the losses in de spectrum based on the parent mass
+            spectrum = add_losses(spectrum)
+            for loss in range(len(spectrum.losses.mz)):
+                # get the losses with 2 decimal points and round down the number
+                rounded_loss = Decimal(spectrum.losses.mz[loss]).quantize(Decimal('.01'),
+                                                                          rounding=ROUND_DOWN)
+                # if a loss in the spectrum contains the loss in the feature then
+                # the document contains the loss!
+                if float(rounded_loss) == float(re.search(r'\_(.*\..{2}).*', feature[0]).group(1)):
+                    num_of_ass_doc_with_feature += 1
+                    documents_that_contain_feature.append(int(document[0]))
+    feature_number = re.search(r'\_(.*)', object[0]).group(1)
+    rounded_feature = Decimal(feature_number).quantize(Decimal('.01'), rounding=ROUND_DOWN)
+    list_of_features.append('%.2f' % float(rounded_feature))
     #if molid is not None, than you can look for mol id in the fragments table
     return None
 #you need the spectrum file with the motif and the identifier
 #you need the file from pdf script where you see the fragments for each motif
+
+def search_for_smiles(list_of_features,list_with_fragments_and_smiles):
+    for feature in list_of_features:
+        if re.search(r'loss', feature) != None:
+
+            spectrum = add_losses(spectrum)
+        for loss in range(len(spectrum.losses.mz)):
+            # get the losses with 2 decimal points and round down the number
+            rounded_loss = Decimal(spectrum.losses.mz[loss]).quantize(Decimal('.01'),
+                                                                      rounding=ROUND_DOWN)
+            # if a loss in the spectrum contains the loss in the feature then
+            # the document contains the loss!
+            feature_number = re.search(r'\_(.*)', feature).group(1)
+            rounded_feature = Decimal(feature_number).quantize(Decimal('.01'), rounding=ROUND_DOWN)
+            #rounded_feature='%.2f' % float(rounded_feature)
+            if float(rounded_loss) == float(re.search(r'\_(.*\..{2}).*', feature[0]).group(1)):
+                num_of_ass_doc_with_feature += 1
+                documents_that_contain_feature.append(int(document[0]))
+    # if the feature we are looking at is a fragment
+    else:
+        for fragment in range(len(spectrum.peaks.mz)):
+            # get the fragment of the spectrum with 2 decimal points and round down the number
+            rounded_fragment = Decimal(spectrum.peaks.mz[fragment]).quantize(Decimal('.01'),
+                                                                             rounding=ROUND_DOWN)
+            # if a fragment in the spectrum is the same as a fragment in the feature
+            # then the document contains the fragment!
+            if float(rounded_fragment) == float(re.search(r'\_(.*\..{2}).*', feature[0]).group(1)):
+                print("hi")
+    return None
 
 
 def main():
@@ -146,7 +223,9 @@ def main():
     path_to_structures_database=argv[1]
     path_to_spectrum_file=argv[2]
     path_to_store_results_db=argv[3]
+    path_to_txt_file_with_motif_and_frag=argv[4]
     identifier="HMDB0000191"
+    current_motif="gnps_motif_38.m2m"
     #knippen in de naam van de spectrum_file
     #identifier,motif=
     #step 1: initialize database to save results
@@ -159,7 +238,15 @@ def main():
     annotate_spectrum(path_to_structures_database, path_to_results_db_file, max_num_break_bonds=10, structure_db="hmdb",
                       ncpus=1)
     # step 4: get identifiers of matches
-    get_molid_of_matched_compound(path_to_results_db_file, identifier)
+    molid=get_molid_of_matched_compound(path_to_results_db_file, identifier)
+    #step 5: search for the features beloning to motif
+    list_of_features=look_for_features(path_to_txt_file_with_motif_and_frag, current_motif)
+    # step 6: get the fragments and smiles of the match
+    if molid is not None:
+        list_with_fragments_and_smiles=make_spectrum_object(molid, path_to_results_db_file)
+        search_for_smiles(list_of_features, list_with_fragments_and_smiles)
+    # step 7:
+
 
 if __name__ == "__main__":
     main()
