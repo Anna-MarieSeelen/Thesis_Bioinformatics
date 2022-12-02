4749015ac1bee5b2f09ea4e3e54059d6a9c9d876
Marcel Seelen <anna-marie.seelen@wur.nl>
2022-12-02 14:23:39 +0100

    adjusted smiles_to_image_script

--- smiles_to_image_file.py
+++ smiles_to_image_file.py
@@ -1,23 +1,21 @@
-
-
 from sys import argv
 from rdkit import Chem
 from rdkit.Chem import Draw
 from rdkit.Chem.Draw import DrawingOptions, MolDrawOptions
 
-def vis_substructure_in_precursor_mol(mol_block):
+def vis_MS2Query_mol(smiles, spectrum_num, motif):
     opts = MolDrawOptions()
     opts.updateAtomPalette({k: (0, 0, 0) for k in DrawingOptions.elemDict.keys()})
-
-    mol = Chem.MolFromMolBlock(mol_block)
-    Draw.MolToFile(mol, f"/lustre/BIF/nobackup/seele006/MAGMa_illustrations_of_substructures/{identifier}_{motif}.png", options=opts)
+    mol = Chem.MolFromSmiles(smiles)
+    Draw.MolToFile(mol, f"/lustre/BIF/nobackup/seele006/MS2Query_identified_structures/{spectrum_num}_{motif}.png", options=opts)
     return None
 
 def main():
-    path_to_store_images=argv[1]
-    list_of_smiles_to_covert_to_img=argv[2]
-    motif=argv[3]
+    print("hiiiiiiiii")
+    smiles=argv[1]
+    motif=argv[2]
+    spectrum_num=argv[3]
+    vis_MS2Query_mol(smiles, spectrum_num, motif)
 
-
-if __name__ == "main":
+if __name__ == "__main__":
     main()
