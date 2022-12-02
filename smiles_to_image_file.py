5e61bbc32f45f6774366c2814d6f3608caf46402
Marcel Seelen <anna-marie.seelen@wur.nl>
2022-12-02 17:10:44 +0100

    tried to add hightlighting the substructure, not working

--- smiles_to_image_file.py
+++ smiles_to_image_file.py
@@ -3,19 +3,41 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 from rdkit.Chem.Draw import DrawingOptions, MolDrawOptions
 
-def vis_MS2Query_mol(smiles, spectrum_num, motif):
+def vis_MS2Query_mol(smiles, spectrum_num, motif, substructure_smiles):
+    # creating an atom list for visualization
+    mol = Chem.MolFromSmiles(smiles)
+    mol_block = Chem.MolToMolBlock(mol)
+    atom_list = list(mol.GetSubstructMatch(substructure_smiles))
+    bond_list=get_bond_list(atom_list, mol_block)
+
     opts = MolDrawOptions()
     opts.updateAtomPalette({k: (0, 0, 0) for k in DrawingOptions.elemDict.keys()})
-    mol = Chem.MolFromSmiles(smiles)
-    Draw.MolToFile(mol, f"/lustre/BIF/nobackup/seele006/MS2Query_identified_structures/{spectrum_num}_{motif}.png", options=opts)
+
+    Draw.MolToFile(mol, f"/lustre/BIF/nobackup/seele006/MS2Query_identified_structures/{spectrum_num}_{motif}.png", highlightAtoms=atom_list, highlightBonds=bond_list, options=opts)
     return None
+
+def get_bond_list(atom_list, molblock):
+    # creating a bond list for visualization
+    mol = Chem.MolFromMolBlock(molblock)
+
+    # creating a bond list for visualization
+    bond_list = []
+    bonds_in_prec_mol = [(x.GetBeginAtomIdx(), x.GetEndAtomIdx()) for x in mol.GetBonds()]
+    for bond in bonds_in_prec_mol:
+        atom_1,atom_2=bond
+        #if atom 1 and atom 2 are also both in the atom_list then they are connected to each other in the molecule!
+        if all(x in atom_list for x in [atom_1,atom_2]):
+            bond_list.append(mol.GetBondBetweenAtoms(atom_1, atom_2).GetIdx())
+    return bond_list
+
 
 def main():
     print("hiiiiiiiii")
     smiles=argv[1]
     motif=argv[2]
     spectrum_num=argv[3]
-    vis_MS2Query_mol(smiles, spectrum_num, motif)
+    substructure_smiles=argv[4]
+    vis_MS2Query_mol(smiles, spectrum_num, motif, substructure_smiles)
 
 if __name__ == "__main__":
     main()
